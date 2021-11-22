548456e7b6120f5b6c124cb32a952e6fa0b3c939
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-11-22 22:24:53 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+const hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Sever is running at port ' + port);
+});
