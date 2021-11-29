a3471e5cb12015e1bcea09a7324dac4f4fd3d385
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-11-29 15:23:38 +0700

    Remove editing from, add new editing page

---
+++ src/models/Account.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Account = new Schema({
+    email: {type:String, required:true},
+    name: {type:String, required:true}
+    // password: {type:String, required:true},
+    // role: {type:String, required:true}
+}); 
+
+module.exports = mongoose.model("Account", Account);
