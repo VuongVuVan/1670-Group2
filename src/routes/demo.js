92f77193ad40b49e4d45106e24b2b7a6517df87d
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-11-30 18:10:58 +0700

    demo upload image function at demo page

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const upload = require("../utils/uploadImage");
 const demoController = require("../controllers/DemoController");
 const path = require("path");
-const currentPath = path.join(__dirname, "../public/uploads/demo");
+const avatarPath = path.join(__dirname, "../public/uploads/demo");
 
 router.get("/search", demoController.search);
 router.get("/delete", demoController.delete);
 router.get("/edit", demoController.edit);
 router.post("/update", demoController.update);
 router.get("/create", demoController.create);
-router.post("/store", upload(currentPath).single("image"), demoController.store);
+router.post("/store", upload(avatarPath).single("image"), demoController.store);
 router.get("/", demoController.show);
 
 module.exports = router;
