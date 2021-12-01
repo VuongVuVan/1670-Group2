9e519f5ba8bb6018a7560de756193f7e94d17e0d
Khanhtrans <83150174+Khanhtrans@users.noreply.github.com>
2021-12-01 22:14:04 +0700

    test

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async() => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/ASM1670", {
+        await mongoose.connect("mongodb://127.0.0.1:27017/GCH0805DB", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- src/utils/imageHandler.js
+++ src/utils/imageHandler.js
@@ -1,33 +1,18 @@
 const multer = require("multer");
-const sharp = require("sharp");
-const fs = require("fs");
 
-exports.resize = (req, res, next) => {
-    console.log("");
-    sharp(req.file.path)
-    .resize(200, 200)
-    .toFile("../", (err, info) => {
-        // fs.unlinkSync(req.file.path);
-        if(!err) console.log("Resize succeed");
-        else next(err);
-    });
-}
-
-exports.upload = destination => {
+function upload(currentPath) {
     const storage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, destination);
+            cb(null, currentPath);
         },
         filename: (req, file, cb) => {
             var index = file.originalname.lastIndexOf(".");
-            var extension = file.originalname.substr(index+1);
-            var image_name = req.body.email.split("@")[0] + "_" +  Date.now() + "." + extension;
+            var extension = file.originalname.substr(index + 1);
+            var image_name = req.body.name + "_" + Date.now() + "." + extension;
             cb(null, image_name);
         }
     });
-    const limits = {
-        fieldSize: 1000000
-    }
-    return multer({storage, limits}).single("image");
+    return multer({ storage: storage });
 }
 
+module.exports = { upload };
