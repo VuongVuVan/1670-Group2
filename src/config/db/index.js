48173936bad2274f140d222e59fb485a0e61df49
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-01 20:28:01 +0700

    update category, course, profile staff

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async() => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/group2", {
+        await mongoose.connect("mongodb://localhost:27017/ASM1670", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
