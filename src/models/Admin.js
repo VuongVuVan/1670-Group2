0eb6c7e20995c784a5a5b07e9aa8e168f1b2c011
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-12-08 17:38:51 +0700

    update admin page

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -5,7 +5,8 @@ const AdminSchema = new Schema({
     email: {type:String, required:true, unique:true},
     image: {
         data: {type:Buffer, required:true},
-        contentType: {type:String, required:true}
+        contentType: {type:String, required:true},
+        name: {type:String}
     },
     name: {type:String, required:true, minlength:2, maxlength:30},
     dob: {type:String, required:true},
