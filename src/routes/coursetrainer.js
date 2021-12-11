866200e3188a11b4488855a58187ec81ac14ef89
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-12-11 15:23:50 +0700

    merge code

--- src/routes/coursetrainer.js
+++ src/routes/coursetrainer.js
@@ -7,12 +7,7 @@ router.get("/delete", coursetrainerController.delete);
 router.get("/edit", coursetrainerController.edit);
 router.post("/update", coursetrainerController.update);
 router.get("/create", coursetrainerController.create);
-<<<<<<< HEAD
-// router.post("/store", img.upload(destination), coursetrainerController.store);
-router.get("/", coursetrainercontroller.show);
-=======
 router.post("/store", coursetrainerController.store);
 router.get("/", coursetrainerController.show);
->>>>>>> dbf9248e9d612868eee3beb0182ecfee101975ff
 
 module.exports = router;
