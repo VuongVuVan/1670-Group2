6cba84cb615c8381f13e8b57ac3f8eb9e1fd80b0
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-12 23:45:28 +0700

    update profile staff, courses layout

--- src/routes/staff.js
+++ src/routes/staff.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const {isStaff} = require("../utils/authHandler");
+const { isStaff } = require("../utils/authHandler");
 const destination2 = path.join(__dirname, "../public/uploads/trainees");
 const img = require("../utils/imageHandler");
 const staffController = require("../controllers/StaffController");
-<<<<<<< HEAD
 const destination = path.join(__dirname, "../public/uploads/staffs");
-=======
->>>>>>> 317a27e3f89bfd345e09f4588633e2f088bb590c
 const width = height = 170;
-const destination = path.join(__dirname, "../public/uploads/staff");
+
 
 router.get("/profile", staffController.show);
 router.get("/edit", staffController.edit);
 router.get("/", staffController.index);
-router.get("/delete", staffController.delete);
+// router.get("/delete", staffController.delete);
 router.post("/update", img.upload(destination), img.resize(width, height), staffController.update);
-router.post("/store", img.upload(destination), img.resize(width, height), staffController.store);
+// router.post("/store", img.upload(destination), img.resize(width, height), staffController.store);
 
 
 /** (Vuong)
@@ -27,12 +24,12 @@ router.post("/store", img.upload(destination), img.resize(width, height), staffC
  *========================================================================================*
  *========================================================================================*
  */
- router.get("/trainee-accounts/search", isStaff, staffController.searchTraineeAccounts);
- router.get("/trainee-accounts/passwords/set_default", isStaff, staffController.setDefaultPassTee);
- router.post("/trainee-accounts/update", isStaff, img.upload(destination2), img.resize(width, height), staffController.updateTraineeAccount);
- router.get("/trainee-accounts/edit", isStaff, staffController.editTraineeAccount);
- router.post("/trainee-accounts/store", isStaff, img.upload(destination2), img.resize(width, height), staffController.storeTraineeAccount);
- router.get("/trainee-accounts/delete", isStaff, staffController.deleteTraineeAccount);
- router.get("/trainee-accounts", isStaff, staffController.showTraineeAccounts); 
+router.get("/trainee-accounts/search", isStaff, staffController.searchTraineeAccounts);
+router.get("/trainee-accounts/passwords/set_default", isStaff, staffController.setDefaultPassTee);
+router.post("/trainee-accounts/update", isStaff, img.upload(destination2), img.resize(width, height), staffController.updateTraineeAccount);
+router.get("/trainee-accounts/edit", isStaff, staffController.editTraineeAccount);
+router.post("/trainee-accounts/store", isStaff, img.upload(destination2), img.resize(width, height), staffController.storeTraineeAccount);
+router.get("/trainee-accounts/delete", isStaff, staffController.deleteTraineeAccount);
+router.get("/trainee-accounts", isStaff, staffController.showTraineeAccounts);
 
 module.exports = router;
