b3dd449c9141afc937ee535876ccde9223c013ce
VuongVuVan <vuongvvgch190692@fpt.edu.vn>
2021-12-15 01:00:12 +0700

    update view to change password

--- src/app.js
+++ src/app.js
@@ -36,38 +36,19 @@ hbs.registerHelper("removeSpace", string => {
 const db = require("./config/db");
 db.connect();
 
-// Use a router for a particular root URL
-
 const demoRouter = require("./routes/demo");
 app.use("/demo", demoRouter);
-
-const categoriesRouter = require("./routes/categories");
-app.use("/categories", categoriesRouter);
-
-const coursesRouter = require("./routes/courses");
-app.use("/courses", coursesRouter);
 
 const adminRouter = require("./routes/admin");
 app.use("/admin", adminRouter);
 
 const staffRouter = require("./routes/staff");
 app.use("/staff", staffRouter);
-
-//Trainee: 
 const traineeRouter = require("./routes/trainee");
 app.use("/trainee", traineeRouter);
 
-const gradeRouter = require("./routes/grade");
-app.use("/grade", gradeRouter);
-
-const seeallRouter = require("./routes/seeall");
-app.use("/seeall", seeallRouter);
-
 const trainerRouter = require("./routes/trainer");
 app.use("/trainer", trainerRouter);
-
-const coursetrainerRouter = require("./routes/coursetrainer");
-app.use("/coursetrainer", coursetrainerRouter);
 
 const siteRouter = require("./routes/site");
 app.use("/", siteRouter);
