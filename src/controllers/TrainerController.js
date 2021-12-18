2527cafa8a26709c90290c71c6d140f3a24459ac
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-18 23:37:13 +0700

    view and edit profile for trainee

--- src/controllers/TrainerController.js
+++ src/controllers/TrainerController.js
@@ -5,58 +5,53 @@ const avatarPath = path.join(__dirname, "../public/uploads/trainer");
 
 class TrainerController {
     index(req, res) {
-        res.render("trainer");
+        res.render("trainer", { user: req.session.user });
     }
 
     layout(req, res) {
         res.render("trainer/layout");
     }
 
-    show(req, res, next) {
-        Trainer.find({}, (err, trainees) => {
-            if (!err) res.render("trainer/profile", { data: trainers });
-            else next(err);
-        });
-    }
+
 
     // create(req, res, next) {
     //     res.render("seeall/create")
     // }
 
-    store(req, res, next) {
-        const obj = {
-            email: req.body.email,
-            name: req.body.name,
-            age: req.body.age,
-            dob: req.body.dob,
-            address: req.body.address,
-            education: req.body.education,
-            image: {
-                data: fs.readFileSync(path.join(destiantion, req.file.filename)),
-                contentType: "image/png"
-            }
-        }
-        console.log(obj)
-        const trainer = new Trainee(obj);
-        trainer.save(err => {
-            if (!err) res.redirect("/trainer/profile");
-            else next(err);
-        });
-    }
+    // store(req, res, next) {
+    //     const obj = {
+    //         email: req.body.email,
+    //         name: req.body.name,
+    //         age: req.body.age,
+    //         dob: req.body.dob,
+    //         address: req.body.address,
+    //         education: req.body.education,
+    //         image: {
+    //             data: fs.readFileSync(path.join(destiantion, req.file.filename)),
+    //             contentType: "image/png"
+    //         }
+    //     }
+    //     console.log(obj)
+    //     const trainer = new Trainee(obj);
+    //     trainer.save(err => {
+    //         if (!err) res.redirect("/trainer/profile");
+    //         else next(err);
+    //     });
+    // }
 
-    edit(req, res, next) {
-        Trainer.findById(req.query.id, (err, trainers) => {
-            if (!err) res.render("trainee/edit", { data: trainers });
-            else next(err);
-        });
-    }
+    // edit(req, res, next) {
+    //     Trainer.findById(req.query.id, (err, trainers) => {
+    //         if (!err) res.render("trainee/edit", { data: trainers });
+    //         else next(err);
+    //     });
+    // }
 
-    update(req, res, next) {
-        Trainer.updateOne({ _id: req.query.id }, req.body, err => {
-            if (!err) res.redirect("trainer/profile");
-            else next(err);
-        });
-    }
+    // update(req, res, next) {
+    //     Trainer.updateOne({ _id: req.query.id }, req.body, err => {
+    //         if (!err) res.redirect("trainer/profile");
+    //         else next(err);
+    //     });
+    // }
 
     // delete(req, res, next) {
     //     Seeall.deleteOne({ _id: req.query.id }, err => {
