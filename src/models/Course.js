436ea8a5f4ad9eb6e5ef5eb13c177c75b1319e5e
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-25 01:49:50 +0700

    update models category, course

--- src/models/Course.js
+++ src/models/Course.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    code: { type: String, unique: true, required: true },
-    name: { type: String, unique: true, required: true, minlength: 2, maxlength: 30 },
-    category: { type: String },
-    description: { type: String },
-    session: { type: Number},
+    code: { type: String, unique: true, required: true, minlength: 4, maxlength: 20 },
+    name: { type: String, unique: true, required: true, minlength: 5, maxlength: 30 },
+    category: { type: String, required: true },
+    description: { type: String, required: true, minlength: 10, maxlength: 50 },
+    session: { type: Number, min: 15, max: 40 },
     createAt: { type: String, required: true },
-    updateAt: { type: String, required: true },
-    trainer: { type: String},
-    total: { type: Number},
+    updateAt: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Course", Course);
