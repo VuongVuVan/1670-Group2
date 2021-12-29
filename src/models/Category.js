bc056626eec12af224d2394e29459f8a46eb79ca
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-29 20:29:14 +0700

    update course, category

--- src/models/Category.js
+++ src/models/Category.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Category = new Schema({
-    name: { type: String, required: true, unique: true, minlength: 5, maxlength: 30 },
-    description: { type: String, required: true, minlength: 10, maxlength: 50 },
+    name: { type: String, required: true, unique: true, minlength: 2, maxlength: 30 },
+    description: { type: String, required: true, minlength: 5, maxlength: 50 },
     total: { type: Number, required: true, min: 15, max: 40 }
 });
 
--- src/models/Course.js
+++ src/models/Course.js
@@ -5,8 +5,8 @@ const Course = new Schema({
     code: { type: String, unique: true, required: true, minlength: 4, maxlength: 20 },
     name: { type: String, unique: true, required: true, minlength: 5, maxlength: 30 },
     category: { type: String, required: true },
-    description: { type: String, required: true, minlength: 10, maxlength: 50 },
-    session: { type: Number, min: 15, max: 40 },
+    description: { type: String, required: true, minlength: 5, maxlength: 50 },
+    session: { type: Number, min: 15, max: 100 },
     createAt: { type: String, required: true },
     updateAt: { type: String, required: true }
 });
