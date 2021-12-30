bae5ee04767a0c30904f1eb1d937edca8733d867
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-30 22:00:44 +0700

    update

--- src/models/Course.js
+++ src/models/Course.js
@@ -5,7 +5,7 @@ const Course = new Schema({
     code: { type: String, unique: true, required: true, minlength: 4, maxlength: 20 },
     name: { type: String, unique: true, required: true, minlength: 5, maxlength: 30 },
     category: { type: String, required: true },
-    description: { type: String, required: true, minlength: 5, maxlength: 50 },
+    description: { type: String, required: true, minlength: 5, maxlength: 100 },
     session: { type: Number, min: 15, max: 100 },
     createAt: { type: String, required: true },
     updateAt: { type: String, required: true }
