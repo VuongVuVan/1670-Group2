f3cf45bd34c264a3cf52785abb514ad252131f6d
hoangson237 <87641750+hoangson237@users.noreply.github.com>
2021-12-30 23:13:39 +0700

    update course, category

--- src/models/Course.js
+++ src/models/Course.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Course = new Schema({
     code: { type: String, unique: true, required: true, minlength: 4, maxlength: 20 },
-    name: { type: String, unique: true, required: true, minlength: 5, maxlength: 30 },
+    name: { type: String, unique: true, required: true, minlength: 5, maxlength: 50 },
     category: { type: String, required: true },
     description: { type: String, required: true, minlength: 5, maxlength: 100 },
     session: { type: Number, min: 15, max: 100 },
